6f41ab412f7190c86137af8c0755756728ae36d7
Mohamed Abukar <mo-abukar10@hotmail.com>
2026-03-01 20:28:25 +0000

    Fix tap CI: fix cia class name, remove duplicate techvaultt, clean up
    - cia.rb: class name 'cia' -> 'Cia' (Homebrew requires CamelCase)
    - cia.rb: moved install to top level, fixed version, added license
    - Removed techvaultt.rb (duplicate class name 'Techvault' conflicting with techvault.rb)
    - Removed Formula/others/ (duplicate techvault.rb causing readall failures)
    - Fixed techvault.rb install method (was using ./configure instead of bin.install)
    - Removed stale text.tct

--- Formula/techvault.rb
+++ Formula/techvault.rb
@@ -1,15 +1,31 @@
 class Techvault < Formula
   desc "CLI tool for the Tech Vault repo"
   homepage "https://github.com/moabukar/tech-vault-cli"
-  url "https://github.com/moabukar/tech-vault-cli/releases/download/v0.1.0/tech-vault-cli_0.1.0_darwin_amd64.tar.gz"
-  sha256 "f682637747e8db579ba9d07f2d3cd9066bf6b8a7b53071e3aaaf1d60aeee9887"
+  version "0.1.0"
   license "MIT"
 
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/moabukar/tech-vault-cli/releases/download/v0.1.0/tech-vault-cli_0.1.0_darwin_amd64.tar.gz"
+      sha256 "f682637747e8db579ba9d07f2d3cd9066bf6b8a7b53071e3aaaf1d60aeee9887"
+    else
+      url "https://github.com/moabukar/tech-vault-cli/releases/download/v0.1.0/tech-vault-cli_0.1.0_darwin_amd64.tar.gz"
+      sha256 "f682637747e8db579ba9d07f2d3cd9066bf6b8a7b53071e3aaaf1d60aeee9887"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/moabukar/tech-vault-cli/releases/download/v0.1.0/tech-vault-cli_0.1.0_linux_amd64.tar.gz"
+      sha256 "19ce2d5a052e25d798e9f0a25a572fc6da86578b6204cd7d9e8f916a75fee92c"
+    end
+  end
+
   def install
-    system "./configure", *std_configure_args, "--disable-silent-rules"
+    bin.install "techvault"
   end
 
   test do
-    system "#{bin}/techvault", "--version"
+    system bin/"techvault", "--version"
   end
 end
