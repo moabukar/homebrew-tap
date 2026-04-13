53656d60a9fa4e1489199f1a48f138f683fc04b3
Mo Abukar <mo-abukar10@hotmail.com>
2026-04-14 00:42:39 +0100

    fix: correct component ordering in techvault formula

--- Formula/techvault.rb
+++ Formula/techvault.rb
@@ -2,8 +2,8 @@ class Techvault < Formula
   desc "CLI tool for the Tech Vault repo"
   homepage "https://github.com/moabukar/tech-vault-cli"
   url "https://github.com/moabukar/tech-vault-cli/releases/download/v0.1.0/tech-vault-cli_0.1.0_darwin_amd64.tar.gz"
-  sha256 "f682637747e8db579ba9d07f2d3cd9066bf6b8a7b53071e3aaaf1d60aeee9887"
   version "0.1.0"
+  sha256 "f682637747e8db579ba9d07f2d3cd9066bf6b8a7b53071e3aaaf1d60aeee9887"
   license "MIT"
 
   def install
